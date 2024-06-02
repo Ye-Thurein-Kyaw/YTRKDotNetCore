02ab3c0305b618cc49589556c2c07c2cc4eb0add
Ye-Thurein-Kyaw <98678228+Ye-Thurein-Kyaw@users.noreply.github.com>
2024-06-02 20:38:09 +0630

    [Complete] NLayer Architecture

---
+++ YTRKDotNetCore.NLayer.BusinessLogic/services/BL_Blog.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YTRKDotNetCore.NLayer.DataAccess.Services;
+using YTRKDotNetCore.NLayer.DataAccess.Models;
+
+namespace YTRKDotNetCore.RestApiWithNLayer.Features.Blog
+{
+    public class BL_Blog
+    {
+        private readonly DA_Blog _dablog;
+
+        public BL_Blog()
+        {
+            _dablog = new DA_Blog();
+        }
+
+
+        public List<BlogModel> GetBlogs()
+        {
+            var lst = _dablog.GetBlogs();
+            return lst;
+        }
+
+        public BlogModel GetBlog(int id)
+        {
+            var item = _dablog.GetBlog(id);
+            return item;
+        }
+
+        public int CreateBlog(BlogModel requestModel)
+        {
+            var result = _dablog.CreateBlog(requestModel);
+            return result;
+        }
+
+        public int UpdateBlog(int id, BlogModel requestModel)
+        {
+
+            var result = _dablog.UpdateBlog(id, requestModel);
+            return result;
+
+        }
+
+        public int PatchBlod(int id, BlogModel requestModel)
+        {
+
+            var result = _dablog.PatchBlog(id, requestModel);
+            return result;
+
+        }
+
+        public int DeleteBlog(int id)
+        {
+            var result = _dablog.DeleteBlog(id);
+            return result;
+        }
+    }
+}
---
+++ YTRKDotNetCore.NLayer.UI/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using YTRKDotNetCore.NLayer.DataAccess.Models;
+using YTRKDotNetCore.RestApiWithNLayer.Features.Blog;
+
+Console.WriteLine("Hello, World!");
+
+BL_Blog bl_Blog = new BL_Blog();
+BlogModel model = new BlogModel() {
+    BlogTitle = "",
+    BlogAuthor = "",
+    BlogContent = "",
+};
+bl_Blog.CreateBlog(model);
+ 
